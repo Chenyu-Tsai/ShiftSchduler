657a5779ad910bc47842cf846b1c7a680aef4948
Chenyu-Tsai <skynova@livemail.tw>
2020-09-24 22:18:04 +0800

    add dev.py

---
+++ dev.py
@@ -0,0 +1,53 @@
+"""
+New algorithm plan to deploy
+"""
+
+arrange_priority = None
+staff = None
+day = None
+shifts = None
+person_num = None
+opt_num = None
+rank1_num = None
+rank12_num = None
+opt_min = None
+min_shift = None
+
+def generate_matrix():
+    """
+    Generate a matrix with 1, then take this matrix to reduce 1 to 0
+    to fit the schedule need.
+    """
+    return None
+
+def handle_day_off():
+    """
+    Given the schudule matrix and the staff information, and turn the
+    desinated day_off position to 0.
+    """
+    return None
+
+def handle_on_duty_but_not_count():
+    """
+    Handle the on duty but not count situation
+    """
+    return None
+
+def reduce_to_min_shift():
+    """
+    reduce the staff's schedule to min demand
+    """
+    return None
+
+def _check_serial_schedule():
+    """
+    Check if the staff has reach the max serial work day
+    """
+    return None
+
+def _random_pick():
+    """
+    Pick staff randomly to arrange the schedule
+    """
+    return None
+
