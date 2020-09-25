78980335e0e22c22400f54048a1e1bec85db7f46
Chenyu-Tsai <skynova@livemail.tw>
2020-09-25 23:46:14 +0800

    update dev.py

--- dev.py
+++ dev.py
@@ -17,6 +17,9 @@ def generate_matrix():
     """
     Generate a matrix with 1, then take this matrix to reduce 1 to 0
     to fit the schedule need.
+    Args:
+
+    Output:
     """
     return None
 
@@ -24,30 +27,45 @@ def handle_day_off():
     """
     Given the schudule matrix and the staff information, and turn the
     desinated day_off position to 0.
+    Args:
+
+    Output:
     """
     return None
 
 def handle_on_duty_but_not_count():
     """
     Handle the on duty but not count situation
+    Args:
+
+    Output:
     """
     return None
 
 def reduce_to_min_shift():
     """
     reduce the staff's schedule to min demand
+    Args:
+
+    Output:
     """
     return None
 
 def _check_serial_schedule():
     """
     Check if the staff has reach the max serial work day
+    Args:
+
+    Output:
     """
     return None
 
 def _random_pick():
     """
     Pick staff randomly to arrange the schedule
+    Args:
+
+    Output:
     """
     return None
 
